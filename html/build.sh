1aea90b51677336728a64157b5cb55fddd475fff
Jeremy Olexa <jolexa@jolexa.net>
2017-04-01 18:20:28 -0500

    Push diagram out to site

--- html/build.sh
+++ html/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cp ../diagram.png .
 line=$(grep -n 'MARKDOWN' pre-index.html | cut -d ":" -f 1)
 { head -n $(($line-1)) pre-index.html; cat ../README.md; tail -n +$((line+1)) \
     pre-index.html; } > index.html
