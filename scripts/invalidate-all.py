214841be3580ee6d5f54d68037baada0e89a0238
Jeremy Olexa <jolexa@jolexa.net>
2017-04-01 16:25:45 -0500

    Less client calls

--- scripts/invalidate-all.py
+++ scripts/invalidate-all.py
@@ -4,20 +4,19 @@ from time import time
 import sys
 import boto3
 
+client = boto3.client('cloudfront')
 def get_id(url):
-    cfrontclient = boto3.client('cloudfront')
     print("get_id args: {0}".format(url))
     # url: asdf.cloudfront.net
     # return: E2134123ASDF
     # where E2134123ASDF is the id of asdf.cloudfront.net
-    paginator = cfrontclient.get_paginator('list_distributions')
+    paginator = client.get_paginator('list_distributions')
     response_iterator = paginator.paginate()
     for i in response_iterator:
         for j in i['DistributionList']['Items']:
             if j['Aliases']['Items'][0] == url:
                 return j['Id']
 
-client = boto3.client('cloudfront')
 response = client.create_invalidation(
         DistributionId=get_id(sys.argv[1]),
         InvalidationBatch={
